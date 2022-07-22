ae5a64ab4330c10437c3971ad893d438905429e7
Augpri <105008671+Augpri@users.noreply.github.com>
2022-07-22 17:14:53 +0530

    Initial commit

---
+++ Image-basedQRcodeScanner.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+
+image = cv2.imread("C:\Users\Pooja Gaikwad\Desktop\My Projects\Barcode\QRcodeimg.jpg")
+
+decodedObjects = pyzbar.decode(image)
+for obj in decodedObjects:
+    #print("Type: QRCODE")
+    #print("Data: b'www.copyassignment.com'")
+    print("Type:", obj.type)
+    print("Data:", obj.data,"\n")
+
+cv2.imshow("Frame",image)
+cv2.waitKey(0)
